8d1b60dff65356d0fed8fa656cf6327cd895eacd
AMAT\e170814 <steven_parrill@amat.com>
2021-05-27 14:37:17 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,11 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
-    const url = 'http://localhost:3000/weather?address=' + location
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
-    fetch(url).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error
